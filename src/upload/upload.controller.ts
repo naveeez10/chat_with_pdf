0d3f8c9acfe5971b4c003a8f752b38cc4375784d
naveeez10 <naviz@incubyte.co>
2024-02-28 13:22:50 +0530

    Fix file reading multer issue

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,18 +7,34 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: '../uploads',
+        filename: (req, file, cb) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          cb(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('File is missing');
     }
     try {
+      file.path;
       const processedData = await this.uploadService.processFile(file.path);
       console.log(processedData);
       return processedData;
